48f5f4c93f0335a4bb68900e4875e69224435cc4
Mansi Varshney <mansivarshney13@gmail.com>
2022-02-28 21:56:30 +0530

    implement AJAX

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,8 +6,12 @@ class Stock < ApplicationRecord
             secret_token: Rails.application.credentials.iex_client[:sandbox_secret_access_key],
             endpoint: 'https://sandbox.iexapis.com/v1'
         )
+        begin 
         # client.price(ticker_symbol)
-        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end
     end
 
 end
